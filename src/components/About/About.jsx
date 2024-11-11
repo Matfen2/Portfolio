69ebb4cf875041b1eaca2af411667daa4c085b13
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2024-11-11 12:04:34 +0100

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,7 @@ function About() {
           </div>
           <div className="col-lg-8 text-section">
             <p className="web">Je suis passionné par le développement front-end et le web design, étant sensibilisé à l&apos;expérience utilisateur et l&apos;accessibilité, je développe des projets centrés autour de l&apos;Humain.</p>
-            <p className="gaming">Motivé par les opportunités offertes par les ESN dans le secteur touristique et les industries liées de près ou de loin au gaming, je souhaite mettre mes compétences au service de solutions innovantes qui améliorent l&apos;expérience des utilisateurs et répondent aux besoins des entreprises.</p>
+            <p className="gaming">Motivé par les opportunités offertes par les ESN dans les secteurs liés, de près ou de loin, au gaming, je souhaite mettre mes compétences au service de solutions innovantes visant à améliorer l&apos;expérience utilisateur et à répondre aux besoins des entreprises.</p>
           </div>
         </div>
       </div>
