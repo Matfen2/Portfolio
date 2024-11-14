5049861806d3edcdf5bac6fe24b82bdba04f97be
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2024-11-14 23:59:27 +0100

    Update btnCV

--- src/components/Present/Present.jsx
+++ src/components/Present/Present.jsx
@@ -6,7 +6,8 @@ function Present() {
       <h2 className="name">Mathieu FENOUIL</h2>
       <h2 className="job">Développeur Front-End</h2>
       <div className="dream">Passionné par le développement web, notamment la partie créative.</div>
-      <button type="button" id="btnCV"><a href="/public/CV Mathieu FENOUIL.pdf" download="CV MATHIEU FENOUIL" className="btn-cv">MON CV</a></button>
+      <button type="button" id="btnCV"><a href="/CV%20Mathieu%20FENOUIL.pdf"
+      download="CV Mathieu FENOUIL" className="btn-cv">MON CV</a></button>
     </div>
   )
 }
